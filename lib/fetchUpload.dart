09aec7c7b5d19c821660d8d31439a8012fbdda59
1234 <samyak.vce18@sot.pdpu.ac.in>
2020-05-18 16:20:39 +0530

    File save at Downloads added.

--- lib/fetchUpload.dart
+++ lib/fetchUpload.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:downloads_path_provider/downloads_path_provider.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'myStorage.dart';
 import 'package:path_provider/path_provider.dart';
@@ -51,26 +54,58 @@ Future<void> getDownloadurl(String roomName,int index)async{
 }
 
 void downloadFile(String url) async {
-  HttpClient client =  HttpClient();
-  var _downloadData = List<int>();
-  var fileSave =  File(await _findLocalPath());
-  client.getUrl(
-      Uri.parse(url))
-      .then((HttpClientRequest request) {
-    return request.close();
-  }).then((HttpClientResponse response) {
-    response.listen((d) => _downloadData.addAll(d),
-        onDone: () {
-          fileSave.writeAsBytes(_downloadData);
-          Fluttertoast.showToast(msg: 'File downloaded at $fileSave');
-        }
+  var status = await Permission.storage.status;
+  if(status.isUndetermined || status.isDenied){
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.storage,
+    ].request();
+  }
+  if(status.isPermanentlyDenied || status.isRestricted){
+    openAppSettings();
+  }
+  if(status.isGranted){
+
+    if(isInitialized==false){
+    await FlutterDownloader.initialize(
+        debug: true // optional: set false to disable printing logs to console
     );
-  });
+    isInitialized=true;
+  }
+
+    var fileSave =  await _findLocalPath();//+Platform.pathSeparator + 'Download';
+    print("filesave is $fileSave");
+    final taskId = await FlutterDownloader.enqueue(
+      url: url,
+      savedDir: fileSave,
+      showNotification: true, // show download progress in status bar (for Android)
+      openFileFromNotification: true, // click on notification to open downloaded file (for Android)
+    );
+    final tasks = await FlutterDownloader.loadTasks();
+    await FlutterDownloader.open(taskId: taskId);
+    Fluttertoast.showToast(msg: 'File downloaded at $fileSave');
+//    HttpClient client =  HttpClient();
+//    var _downloadData = List<int>();
+//    var fileSave =  File(await _findLocalPath());
+//    print("$fileSave --is path");
+//    client.getUrl(
+//        Uri.parse(url))
+//        .then((HttpClientRequest request) {
+//      return request.close();
+//    }).then((HttpClientResponse response) {
+//      response.listen((d) => _downloadData.addAll(d),
+//          onDone: () {
+//            fileSave.writeAsBytes(_downloadData);
+//            Fluttertoast.showToast(msg: 'File downloaded at $fileSave');
+//            print("'File downloaded at $fileSave'");
+//          }
+//      );
+//    });
+  }
 }
 
 Future<String> _findLocalPath() async {
-  final directory = await getExternalStorageDirectory();
-  return directory.path +'/docshelper${getTimeStamp()}.pdf';
+  final directory=await DownloadsPathProvider.downloadsDirectory;
+  return directory.path ;//+'/docshelper${getTimeStamp()}.pdf';
 }
 
 
